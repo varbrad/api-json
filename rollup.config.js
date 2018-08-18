01fc1e4ac84c5a62bc4151743c26672c950dae1b
Brad Davies <varbrad@users.noreply.github.com>
2018-08-18 23:57:23 +0100

    Generation code

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import pkg from './package.json'
+
+const input = 'src/main.js'
+
+export default [
+  {
+    input,
+    output: {
+      name: 'jsonApi',
+      file: pkg.browser,
+      format: 'umd'
+    }
+  },
+  {
+    input,
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' }
+    ]
+  }
+]
